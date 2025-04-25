088c153864daefd936f6e4da25a4cc14a1e2273a
Arno <vdmarno@gmail.com>
2025-04-25 18:25:25 +0200

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="text-2xl font-bold text-blue-700">
+      ⚡ Family Energy App
+    </header>
+  );
+}
